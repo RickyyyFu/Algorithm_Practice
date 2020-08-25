888492c6199c9e067f919be530d9cdf166b6eb74
Ricky Fu <curly_fy@outlook.com>
2020-08-25 15:24:14 -0400

    Hex to decimal - parseInt(str, 16)

---
+++ HexToDecimalValid/HexToDecimalValid.java
@@ -0,0 +1,41 @@
+package HexToDecimalValid;
+
+import java.util.*;
+
+public class HexToDecimalValid {
+	public static String isValid() {
+		try (Scanner scanner = new Scanner(System.in)) {
+			String hex_str = scanner.nextLine().toUpperCase();
+			
+			if(hex_str.length() != 8) {
+				System.out.println("INVALID");
+				return "INVALID";
+			}
+			
+			for(char c : hex_str.toCharArray()) {
+				if(Character.isDigit(c)) continue;
+				if(c >= 'A' && c <= 'F') continue;
+				System.out.println("INVALID");
+				return "INVALID";
+			}
+			String first_six = hex_str.substring(0, 6);
+			String last_two = hex_str.substring(6);
+			int decimal = Integer.parseInt(first_six, 16);
+			int sum = 0;
+			while(decimal > 0) {
+				sum += (decimal % 10);
+				decimal /= 10;
+			}
+			if(sum == Integer.parseInt(last_two, 16)) {
+				System.out.println("VALID");
+				return "VALID";
+			}
+			System.out.println("INVALID");
+			return "INVALID";
+		}
+	}
+	
+	public static void main(String[] args) {
+		isValid();
+	}
+}
